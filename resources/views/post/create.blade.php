c76479413942949c8081c7f72af0a851efe03593
FabioBollettini <bollettinifabio@yahoo.com>
2025-09-08 01:05:28 +0200

    create post view

---
+++ resources/views/post/create.blade.php
@@ -0,0 +1,24 @@
+<x-app-layout>
+
+    <div class="py-5">
+        <div class="max-w-5xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-8">
+                <form action="{{ route('post.store') }}" method="post">
+                    <!-- Title -->
+                    <div>
+                        <x-input-label for="title" :value="__('Title')" />
+                        <x-text-input id="title" class="block mt-1 w-full" type="title" name="title" :value="old('title')" required autofocus />
+                        <x-input-error :messages="$errors->get('title')" class="mt-2" />
+                    </div>
+
+                    <!-- Content -->
+                    <div class="mt-4">
+                        <x-input-label for="content" :value="__('Content')" />
+                        <x-input-textarea id="content" class="block mt-1 w-full" type="content" name="content" :value="old('content')" required />
+                        <x-input-error :messages="$errors->get('content')" class="mt-2" />
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -1,9 +1,4 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
 
     <div class="py-5">
         <div class="max-w-5xl mx-auto sm:px-6 lg:px-8">
